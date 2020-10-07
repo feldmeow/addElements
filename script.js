030de51f729ba773ea930bb155fb00e38d7ae4ba
Jauhen Feldman <feldmeow@gmail.com>
2020-10-07 14:09:28 +0300

    add additional

--- script.js
+++ script.js
@@ -2,30 +2,54 @@
 
 // конструктор
 function DomElement() {
-  this.selector = prompt('Введите селектор', '.fff');
-  this.height = prompt('Высота', '500') + 'px'
-  this.width = prompt('Ширина', '1000') + 'px';
-  this.bg = prompt('Цвет блока? (green, red, grey, etc.)', 'grey');
-  this.fontsize = prompt('Размер шрифта', '33') + 'px'
-  this.createElement = function () {
-    if (this.selector[0] === '.') {
-      this.elem = document.createElement('div');
-      this.elem.className = this.selector.slice(1);
-      this.elem.innerHTML = 'DIV'
-    } else {
-      this.elem = document.createElement('p');
-      this.elem.id = this.selector.slice(1);
-      this.elem.innerHTML = 'PARAGRAPH'
-    }
-    this.elem.style.backgroundColor = this.bg;
-    this.elem.style.width = this.width;
-    this.elem.style.height = this.height;
-    this.elem.style.fontSize = this.fontsize;
-    document.body.append(this.elem);
-  };
+  this.selector = 'div'
+  this.height = '100px'
+  this.width = '100px'
+  this.bg = 'green'
+};
+DomElement.prototype.createElement = function () {
+  this.elem = document.createElement(this.selector);
+  this.elem.style.backgroundColor = this.bg;
+  this.elem.style.width = this.width;
+  this.elem.style.height = this.height;
+  this.elem.style.position = 'absolute';
+  document.body.append(this.elem);
 
 };
 // наследование
-let NewElem = new DomElement();
+let newElem = new DomElement();
 // вызов 
-NewElem.createElement();
+document.addEventListener('DOMContentLoaded', newElem.createElement());
+// newElem.createElement();
+let leftPos = 0;
+let topPos = 0;
+function posPlus(pos) {
+  pos = pos + 10;
+  return pos;
+}
+function posDown(pos) {
+  pos = pos - 10
+}
+document.addEventListener('keydown', function (event) {
+
+  function move() {
+    if (event.code == 'ArrowRight') {
+      leftPos = leftPos + 10;
+      newElem.elem.style.left = `${leftPos}px`;
+      console.log(`${leftPos}px`);
+    } else if (event.code == 'ArrowLeft') {
+      leftPos = leftPos - 10;
+      newElem.elem.style.left = `${leftPos}px`;
+      console.log(`${leftPos}px`);
+    } else if (event.code == 'ArrowUp') {
+      topPos = topPos - 10;
+      newElem.elem.style.top = `${topPos}px`;
+      console.log(`${topPos}px`);
+    } else if (event.code == 'ArrowDown') {
+      topPos = topPos + 10;
+      newElem.elem.style.top = `${topPos}px`;
+      console.log(`${topPos}px`);
+    }
+  };
+  move()
+});
